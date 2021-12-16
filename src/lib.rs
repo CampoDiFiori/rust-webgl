955e77583e5d3d4a372086053bb19b8740e4e6af
CampoDiFiori <dudkopiotr@gmail.com>
2021-12-16 13:24:00 +0100

    Initial boilerplate

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub struct DougsClient {}
+
+#[wasm_bindgen]
+impl DougsClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        log("Constructor was hit");
+        Self {}
+    }
+
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        Ok(())
+    }
+
+    pub fn render(&self) {}
+}
